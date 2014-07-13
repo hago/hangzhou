5ea43f0d9d4bd3ca98d11c3ef190d959291c85be
hago sun <suncjs@gmail.com>
2014-07-13 18:05:26 +0800

    fix numpad and incorrect number issue

--- wuye/wuye/wuye/RegisterViewController.m
+++ wuye/wuye/wuye/RegisterViewController.m
@@ -33,6 +33,24 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    UIToolbar* numberToolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 50)];
+    numberToolbar.barStyle = UIBarStyleBlackTranslucent;
+    numberToolbar.items = [NSArray arrayWithObjects:
+                           [[UIBarButtonItem alloc]initWithTitle:NSLocalizedString(@"KeyboardCancel", @"Cancel") style:UIBarButtonItemStyleBordered target:self action:@selector(cancelNumberPad)],
+                           [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
+                           [[UIBarButtonItem alloc]initWithTitle:NSLocalizedString(@"KeyboardDone", @"Done") style:UIBarButtonItemStyleDone target:self action:@selector(doneWithNumberPad)],
+                           nil];
+    [numberToolbar sizeToFit];
+    self.txtcell.inputAccessoryView = numberToolbar;
+}
+
+-(void)cancelNumberPad{
+    [self.txtcell resignFirstResponder];
+    self.txtcell.text = @"";
+}
+
+-(void)doneWithNumberPad{
+    [self.txtcell resignFirstResponder];
 }
 
 - (void)didReceiveMemoryWarning
@@ -77,6 +95,7 @@
     } onFail:^(NSError *error) {
         [Utilities stopLoadingUI];
         NSLog(@"wuye reg fail");
+        [[[UIAlertView alloc] initWithTitle:@"" message:@"电话号码验证不通过" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
     }];
 }
 
